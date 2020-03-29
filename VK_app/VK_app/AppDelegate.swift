855f3cba72f2458bca43e0edf7a27957f65bdc12
DmitriyBalyk <battle1992@yandex.ru>
2020-03-29 18:09:02 +0300

    Lesson_2_DmitriyBalyk

---
+++ VK_app/VK_app/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  VK_lesson1
+//
+//  Created by Дмитрий Балык on 25.03.2020.
+//  Copyright © 2020 DmitriyBalyk. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
